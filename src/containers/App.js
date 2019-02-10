4931615c1d9f649dcdc286b380ace55358cecfb9
Daniel Sidabutar <aguspermadi94@gmail.com>
2019-02-11 01:57:01 +0700

    Refactoring project to intoduce useState hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
-  state = { resource: 'posts' };
-
-  onChanges(resource) {
-    this.setState({ resource });
-  }
-  render () {
-    return (
+const App = () => {
+  const [ resource, setResource ] = useState('posts');
+  
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => this.onChanges('posts')}>Posts</button>
-          <button onClick={() => this.onChanges('todos')}>Todos</button>
-        </div>
-        {
-          this.state.resource
-        }
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
       </div>
-    )
-  }
+      {
+        resource
+      }
+    </div>
+  )
 };
 
 export default App;
